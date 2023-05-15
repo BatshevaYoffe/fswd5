282fc876e0df6d3bb105d2eebecdcaab681b2897
BatshevaYoffe <73179986+BatshevaYoffe@users.noreply.github.com>
2023-05-15 12:18:33 +0300

    gh

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -3,97 +3,100 @@ import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
+// const [inputs, setInputs] = useState({});
 
-class Login extends React.Component {
+// class Login extends React.Component {
 
-    constructor(props) {
-      super(props)
-    //   const navigate = useNavigate();
-      this.state = {name: '',pass:''};
+//     constructor(props) {
+//       super(props)
+//     //   const navigate = useNavigate();
+//     //   const [name, setName] = useState("");
+//       this.state = {name: '',pass:''};
 
-      this.handleChangePass = this.handleChangePass.bind(this)
-      this.handleChangeName = this.handleChangeName.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    // f = () => navigate("/login");
+//       this.handleChangePass = this.handleChangePass.bind(this)
+//       this.handleChangeName = this.handleChangeName.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+//     // f = () => navigate("/login");
 
 
-    handleChangeName(event) {
-        this.setState({name: event.target.name});
-      }
-      handleChangePass(event) {
-        this.setState({pass: event.target.pass});
-      }
-    handleSubmit (event) {
-        // Prevent page reload
-        //event.preventDefault();
-        alert('A name was submitted: ' + this.state.name);
-        alert('A name was submitted: ' + this.state.pass);
-        event.preventDefault();
-      }
-    
+// handleChangeName(event) {
+//     this.setState({name: event.target.name});
+//   }
+//   handleChangePass(event) {
+//     this.setState({pass: event.target.pass});
+//   }
+// handleSubmit (event) {
+//     // Prevent page reload
+//     //event.preventDefault();
+//     alert('A name was submitted: ' + this.state.name);
+//     alert('A name was submitted: ' + this.state.pass);
+//     event.preventDefault();
+//   }
 
-    render() {
-      return    ( 
-        <div>
-      <div className="form">
-        <form onSubmit={this.handleSubmit}>
-          <div className="input-container">
-            <label>Username </label>
-            <input type="text" value={this.state.name} onChange={this.handleChangeName} />
-          </div>
-          <div className="input-container">
-            <label>Password </label>
-            <input type="password" value={this.state.pass} onChange={this.handleChangePass} />
-          </div>
-          <div className="button-container">
-            <input type="submit" value="Submit" />
-          </div>
-        </form>  
-      </div>
-      <div>
-        login
-        <Link to="/content">login</Link>
-      </div>
-    </div>
-    );}}export default Login;
 
-// const Login = () => {
-//     const [g,m]=useState();
-//     const navigate = useNavigate();
-//     const f = () => navigate("/login");
-//     const  handleSubmit = (event) => {
-//         console.log("handleSubmit");
-//         // Prevent page reload
-//         event.preventDefault();
-//         var { uname, pass } = document.forms[0];
-//         console.log(uname.text);
-
-//       };
-//     return <div>
+//     render() {
+//       return    ( 
+//         <div>
 //       <div className="form">
-//         <form 
-//           onSubmit={handleSubmit}>
+//         <form onSubmit={this.handleSubmit}>
 //           <div className="input-container">
 //             <label>Username </label>
-//             <input type="text" name="uname" required />
-//             {/* {renderErrorMessage("uname")} */}
+//             <input type="text" name='usname' value={inputs.usname} onChange={this.handleChangeName}  />
 //           </div>
 //           <div className="input-container">
 //             <label>Password </label>
-//             <input type="password" name="pass" required />
-//             {/* {renderErrorMessage("pass")} */}
+//             <input type="password" value={this.state.pass} onChange={this.handleChangePass} />
 //           </div>
 //           <div className="button-container">
-//             <input type="submit" />
+//             <input type="submit" value="Submit" />
 //           </div>
-//         </form>
-//         <button type="button" onClick={f}>fdgh</button>
-  
+//         </form>  
 //       </div>
 //       <div>
 //         login
 //         <Link to="/content">login</Link>
 //       </div>
 //     </div>
-//   };export default Login;
+//     );}}export default Login;
+
+const Login = () => {
+
+    const [inputs, setInputs] = useState({});
+    const navigate = useNavigate();
+    const f = () => navigate("/login");
+    const handleChangeName = (event) => {
+        const name = event.target.username;
+        const value = event.target.password;
+        setInputs(values => ({ ...values, [name]: value }))
+    }
+    // const handleChangePass = (event) => {
+    //     this.setState({ pass: event.target.pass });
+    // }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert(inputs);
+
+    }
+    return <div>
+        <div className="form">
+            <form onSubmit={handleSubmit}>
+                <div className="input-container">
+                    <label>Username </label>
+                    <input type="text" name="username" value={inputs.username} onChange={handleChangeName} />
+                </div>
+                <div className="input-container">
+                    <label>Password </label>
+                    <input type="password" name="password" value={inputs.password} onChange={handleChangeName} />
+                </div>
+                <div className="button-container">
+                    <input type="submit" value="Submit" />
+                </div>
+            </form>
+        </div>
+        <div>
+            login
+            <Link to="/content">login</Link>
+        </div>
+    </div>
+}; export default Login;
