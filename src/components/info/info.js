5e011aa234f3f94ae18b1d68c420f0af20c2ef49
BatshevaYoffe <73179986+BatshevaYoffe@users.noreply.github.com>
2023-05-17 11:03:17 +0300

    info

--- src/components/info/info.js
+++ src/components/info/info.js
@@ -5,39 +5,36 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 
-const Info = (props) => {
+
+
+
+const Info = () => {
     const [user, setMyObject] = useState({});
 
     useEffect(() => {
         const objectData = JSON.parse(localStorage.getItem('myData'));
+        console.log(objectData);
         setMyObject(objectData);
     }, []);
 
     return(
-        <Card style={{ width: '18rem' }}>
-          <Card.Body>
-            <Card.Title>{user.name}</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">{user.username}</Card.Subtitle>
-            <Card.Text>
-              <p>Email: {user.email}</p>
-              <p>Phone: {user.phone}</p>
-              <p>Website: {user.website}</p>
-            </Card.Text>
-            <Card.Title>Address:</Card.Title>
-            {/* <ListGroup className="list-group-flush">
-              <ListGroupItem>{user.address.street}, {user.address.suite}</ListGroupItem>
-              <ListGroupItem>{user.address.city}, {user.address.zipcode}</ListGroupItem>
-              <ListGroupItem>Lat: {user.address.geo.lat}, Lng: {user.address.geo.lng}</ListGroupItem>
-            </ListGroup> */}
-            <Card.Title>Company:</Card.Title>
-            <Card.Text>
-              {/* <p>{user.company.name}</p>
-              <p>{user.company.catchPhrase}</p>
-              <p>{user.company.bs}</p> */}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-    );
+        <div>
+          <h1>{user.name}</h1>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+          <p>Website: {user.website}</p>
+          <h2>Address:</h2>
+          <p>{user.address.street}</p>
+        <p>{user.address.suite}</p>
+          <p>{user.address.city}</p><p> {user.address.zipcode}</p>
+          <p>Lat: {user.address.geo.lat}</p><p> Lng: {user.address.geo.lng}</p>
+          <h2>Company:</h2>
+          <p>{user.company.name}</p>
+          <p>{user.company.catchPhrase}</p>
+          <p>{user.company.bs}</p> 
+        </div>
+      );
 }
 export default Info;
 
