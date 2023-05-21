62fa426562e62fb998c6b7c1840086dbc6f425a8
popper <popper@LAPTOP-MF8PAB5D>
2023-05-21 11:42:48 +0300

    פוסט

--- src/components/posts/post.js
+++ src/components/posts/post.js
@@ -1,12 +1,42 @@
-import React, { useState } from 'react';
+
 //import './style.css';
-import { Outlet, json } from 'react-router-dom';
+import { Outlet, json, useLocation } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
-const Post = () => {
+const Post = (props) => {
+    //exrtact user from local storage
+    const [user, setMyObject] = useState({});
+    useEffect(() => {
+        const objectData = JSON.parse(localStorage.getItem('myData'));
+        setMyObject(objectData);
+        console.log("user");
+    }, []);
+    // all posts for this user
+    const [activeUsers, setActiveUsers] = useState([]);
+
+    useEffect(() => {
+
+        let id=user.id;
+        fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
+            .then(response => response.json())
+            .then(data => {
+                // const activeUsersData = data.filter(user => user.user.id);
+                console.log(data);
+                setActiveUsers(data);
+            })
+            .catch(error => console.log(error));
+    }, [user]); // dependency for user
 
     return <div>
-Postttttttt
+        <ul>
+                {activeUsers.map(activeUser => (
+                    <li key={activeUser.userId}>
+                        
+                        <span>{activeUser.title}</span>
+                    </li>
+                ))}
+            </ul>
     </div>
 }; export default Post;
